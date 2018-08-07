b2f791516763feb0b79fe2822c35da2c5ac27fc1
AnnieGregg <annie.r.gregg@gmail.com>
2018-08-07 21:19:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,4 @@
-
+for n in 1..500
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
@@ -6,5 +6,6 @@ def fizzbuzz(number)
     return "Fizz"
   elsif number % 5 == 0
    return "Buzz"
+ else puts "#{n}"
   end
 end
